579be60aa0cebdd298f7be7b81da902b8fab7591
yawnit <tin.systems.mac@gmail.com>
2022-07-05 22:47:08 +0300

    cros test

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,23 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+ 
 Route::get('/', function () {
     return 'test';
+});
+
+// Вареант 1
+
+Route::get('/test',  function  (Request $request)  {
+    return response()->json(['CORS']);
+});
+
+// Вареант 2
+
+Route::middleware(['cors'])->group(function () {
+    Route::get('/test2',  function  (Request $request)  {
+        return response()->json(['CORS 2']);
+    });
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
