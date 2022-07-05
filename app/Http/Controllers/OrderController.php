f3bd04f225adc6725f128a76048b3d11eb9d64ff
yawnit <tin.systems.mac@gmail.com>
2022-07-06 00:19:40 +0300

    add edit order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,12 +9,6 @@ use Carbon\Carbon;
 class OrderController extends Controller
 {
 
-    public static function test(Request $request)  {
-
-        return response()->json(DB::connection()->select('select * from test')->get());
-    }
-
-
     public static function create(Request $request)  {
 
         $request->validate([
@@ -26,17 +20,41 @@ class OrderController extends Controller
 
         $date = Carbon::now();
 
-        DB::table('order')->insert([
+        $id = DB::table('order')->insertGetId([
             'phone' => $request->phone,
             'fio' => $request->fio,
             'amount' => $request->amount,
             'address' => $request->address,
             'date' => $date,
         ]);
+        
+        return response()->json(['status'=>true, 'orderId' => $id]);
 
-        return $date;
+    }
 
-        // return response()->json(DB::connection()->select('select * from test')->get());
+
+    public static function edit(Request $request)  {
+
+        $request->validate([
+            'orderId' => 'required|int',
+            'phone' => 'required|int',
+            'fio' => 'required|string',
+            'amount' => 'required|int',
+            'address' => 'required|string',
+        ]);
+
+        $date = Carbon::now();
+
+        DB::table('order')->where('id', $request->orderId)->update([
+            'phone' => $request->phone,
+            'fio' => $request->fio,
+            'amount' => $request->amount,
+            'address' => $request->address,
+            'date' => $date,
+        ]);
+        
+        return response()->json(['status'=>true, 'orderId' => $request->orderId]);
+
     }
 
 }
